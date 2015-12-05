e4c243d4a2f34ce8c573b81d95582e3dd4b0dac0
Wayne Hutchinson <wfh2727@gmail.com>
2015-12-04 19:36:54 -0600

    Removed console logs

--- javascripts/modal.js
+++ javascripts/modal.js
@@ -7,9 +7,7 @@ define(function(require) {
 
   $(document).on("click", ".review-button", function() {
     var btnId = $(this).attr('id');
-    console.log(btnId);
     $("." + btnId).css("display", "block");
-    console.log("took trip button clicked");
   });
 
   $(document).on("click", "#cancel-btn", function() {
@@ -18,23 +16,18 @@ define(function(require) {
 
   $(document).on("click", ".remove-list", function() {
     var delKey = $(this).attr("data-btn");
-    console.log(delKey);
     var myFireBaseRef = new Firebase('https://nss-wayne-trippin.firebaseio.com/trips/' + delKey);
     myFireBaseRef.remove();
   });
 
   $("body").on("click", "#submit-review-btn", function() {
-    console.log("submit clicked");
 
     var year = $("#add-year").val();
     var month = $("#add-month").val();
     var review = $("#add-review").val();
     var key = $(this).attr("data-key");
 
-    console.log(year, month, review, key);
-
     var myFireBaseRef = new Firebase('https://nss-wayne-trippin.firebaseio.com/trips/' + key);
-    console.log(myFireBaseRef);
     myFireBaseRef.update({ year: year, month: month, review: review, visited: true });
 
     location.reload();

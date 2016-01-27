ff1f316e5793174c5db5d9c65c6bdcb46edc354d
Wayne Hutchinson <wfh2727@gmail.com>
2016-01-26 20:51:07 -0600

    Fixed bug when added a trip to taken list

--- javascripts/modal.js
+++ javascripts/modal.js
@@ -22,10 +22,17 @@ define(function(require) {
 
   $("body").on("click", "#submit-review-btn", function() {
 
-    var year = $("#add-year").val();
-    var month = $("#add-month").val();
-    var review = $("#add-review").val();
+    
     var key = $(this).attr("data-key");
+    var year = $("#add-year" + key).val();
+    var month = $("#add-month" + key).val();
+    var review = $("#add-review" + key).val();
+    
+    console.log("update year ", year);
+    console.log("update month ", month);
+    console.log("update review ", review);
+    console.log("update trip key = ", key);
+
 
     var myFireBaseRef = new Firebase('https://nss-wayne-trippin.firebaseio.com/trips/' + key);
     myFireBaseRef.update({ year: year, month: month, review: review, visited: true });
